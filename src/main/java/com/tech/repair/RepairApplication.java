f3875f2ed70884989ca8b64dfe62aca1758ed0cd
bcu-studio <bcu@studio.com>
2019-09-15 22:12:57 +0800

    demo

--- src/main/java/com/tech/repair/RepairApplication.java
+++ src/main/java/com/tech/repair/RepairApplication.java
@@ -2,9 +2,18 @@ package com.tech.repair;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RepairApplication {
+public class RepairApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(RepairApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(RepairApplication.class, args);
